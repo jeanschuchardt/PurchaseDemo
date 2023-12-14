e4192c0bf533b249755cdf857f13aa90427dbbeb
Jean Schuchardt <jeanschuchardt@hotmail.com>
2023-12-13 22:19:34 -0300

    create entity

--- src/main/java/com/jean/burda/purchase_service/entity/PurchaseTransaction.java
+++ src/main/java/com/jean/burda/purchase_service/entity/PurchaseTransaction.java
@@ -1,4 +1,25 @@
 package com.jean.burda.purchase_service.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Entity
+@Data
 public class PurchaseTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private LocalDate transactionDate;
+    private BigDecimal purchaseAmount;
+
+
 }
